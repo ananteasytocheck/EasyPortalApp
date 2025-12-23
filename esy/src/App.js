d496ea1e23d75fe743e43f6195607fd6c8907ee0
ananteasytocheck <anant@easytocheck.com>
2025-12-23 11:57:38 +0530

    secondary changes

--- esy/src/App.js
+++ esy/src/App.js
@@ -4,6 +4,7 @@ const App = () => {
   return (
     <div>
       <h1 className='text-3xl text-red-500'>App</h1>
+      <p className='text-3xl text-blue-800'>Hello how are you Anant?</p>
     </div>
   )
 }
